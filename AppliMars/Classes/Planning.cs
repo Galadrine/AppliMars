351756b61471703dc867331b58a997bb72e683e8
JackAndros <alex.barret92@gmail.com>
2015-11-18 19:20:08 +0100

    Ajout Fenetre niveau 3

--- AppliMars/Classes/Planning.cs
+++ AppliMars/Classes/Planning.cs
@@ -30,6 +30,7 @@ public class Planning
 
 	public Planning()
 	{
+
 	}
 
 }
---
+++ AppliMars/WindowLevel3.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppliMars {
+    public partial class WindowLevel3 : Form {
+        public WindowLevel3() {
+            InitializeComponent();
+        }
+    }
+}
