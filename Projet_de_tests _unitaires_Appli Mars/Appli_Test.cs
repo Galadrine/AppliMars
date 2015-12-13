67b95ec6b070f3275de5bc7a220e119e445f87d3
cmoscet <carl.moscet@gmail.com>
2015-12-13 15:32:21 +0100

    Création d'un projet de tests unitaires dans la Solution

---
+++ Projet_de_tests _unitaires_Appli Mars/Appli_Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AppliMars;
+
+namespace Appli_Mars_Tests
+{
+    [TestClass]
+    public class Appli_Test
+    {
+        [TestMethod]
+        public void initAct()
+        {
+
+        }
+    }
+}
