735a03611329900b3c165da95026c2ba0185f296
cmoscet <carl.moscet@gmail.com>
2015-12-17 18:09:54 +0100

    Tests Unitaires Classes  & Debut Window

---
+++ Projet_de_tests _unitaires_Appli Mars/Appli_Test_Window.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AppliMars;
+using System.Collections.Generic;
+using System.Linq;
+//using System.Xml.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Projet_de_tests__unitaires_Appli_Mars
+{
+    [TestClass]
+    class Appli_Test_Window
+    {
+        [TestMethod]
+        ////////////////////////////// Test Window Level0 ///////////////////
+        //public partial class WindowLevel0 : Form 
+        //{
+        //WindowLevel0.refreshListBox();
+        //}
+
+        /////////////////////////////// Test Window Level1 //////////////////
+
+        //private List<Button> _boutons = new List<Button>();
+        //public List<Button> genererBoutonsJours();
+
+    }
+}
