aef221c82bbc91fee857abe9795466d0f653af35
cmoscet <carl.moscet@gmail.com>
2015-12-17 16:31:21 +0100

    Tests Unitaires
    Tests Unitaires des Classes

--- Projet_de_tests _unitaires_Appli Mars/Appli_Test.cs
+++ Projet_de_tests _unitaires_Appli Mars/Appli_Test.cs
@@ -1,6 +1,11 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using AppliMars;
+using System.Collections.Generic;
+using System.Linq;
+using System.Drawing;
+using System.Text;
+
 
 namespace Appli_Mars_Tests
 {
@@ -8,9 +13,94 @@ namespace Appli_Mars_Tests
     public class Appli_Test
     {
         [TestMethod]
-        public void initAct()
-        {
+        ///////////////////////////////////// Test Classe Journee ////////////////////////////////
 
-        }
+        //public void Test_Journee_getPosActiviteByAct()
+        //{
+        //    Mission Tests = new Mission("Tests", 17 / 12 / 2015, 500, "Moscet", "1");
+        //    Journee J = new Journee(1, "Eat,Sleep,Rave,Repeat", Tests);
+        //    J.getPosActiviteByIdAct(1);
+        //}
+        //public void Test_Journee_checkJourneeExterieur()
+        //{
+        //    Mission Tests = new Mission("Tests", 17 / 12 / 2015, 500, "Moscet", "1");
+        //    Journee J = new Journee(1, "Eat,Sleep,Rave,Repeat", Tests);
+        //    J.checkJourneeExterieur();
+        //}
+
+        ////////////////////////////////////// Test Classe Mission ////////////////////////////////
+
+        //public void Test_Mission_calculJourJ()
+        //{
+        //    Mission Tests = new Mission("Tests", 17 / 12 / 2015, 500, "Moscet", "1");
+        //    Tests.calculJourJ();
+        //}
+
+        //public void Test_Mission_creaPlanningXML()
+        //{
+        //    Mission Tests = new Mission("Tests", 17 / 12 / 2015, 500, "Moscet", "1");
+        //    XDocument generalXML = new XDocument();
+        //    Tests.creaPlanningXML(generalXML);
+        //}
+
+        //////////////////////////////////// Test Classe Categorie ////////////////////////////////
+
+        //public void Test_Categorie_ajoutSousCategorie()
+        //{
+        //    Categorie Matin = new Categorie("Matin");
+        //    Categorie Petit_Dejeuner = new Categorie("Petit Dejeuner");
+        //    Matin.ajoutSousCategorie(Petit_Dejeuner);
+        //}
+
+        ////////////////////////////////////// Test Classe Activite ////////////////////////////////
+        //public void Test_Activite_initAct()
+        //{
+        //    Astronaute Moscet = new Astronaute("Moscet");
+        //    Astronaute Barret = new Astronaute("Barret");
+        //    Astronaute Lydie = new Astronaute("Lydie");
+        //    List <Astronaute> participants = new List<Astronaute>();
+        //    participants.Add(Moscet);
+        //    participants.Add(Barret);
+        //    participants.Add(Lydie);
+        //    Activite Act = new Activite("Sleeping", false, "Description", 9, 9, 14, 14, participants);
+        //    Act.initAct("Eating", true, "Description", 9, 9, 14, 14, participants);
+        //}
+
+        //public void Test_Activite_getDetailsActivites()
+        //{
+        //    Astronaute Moscet = new Astronaute("Moscet");
+        //    Astronaute Barret = new Astronaute("Barret");
+        //    Astronaute Lydie = new Astronaute("Lydie");
+        //    List<Astronaute> participants = new List<Astronaute>();
+        //    participants.Add(Moscet);
+        //    participants.Add(Barret);
+        //    participants.Add(Lydie);
+        //    Activite Act = new Activite("Sleeping", false, "Description", 9, 9, 14, 14, participants);
+        //    Act.getDetailsActivites();
+        //}
+
+        //public void Test_Activite_convertionCoordonneesXML()
+        //{
+        //    Astronaute Moscet = new Astronaute("Moscet");
+        //    Astronaute Barret = new Astronaute("Barret");
+        //    Astronaute Lydie = new Astronaute("Lydie");
+        //    List<Astronaute> participants = new List<Astronaute>();
+        //    participants.Add(Moscet);
+        //    participants.Add(Barret);
+        //    participants.Add(Lydie);
+        //    Activite Act = new Activite("Sleeping", false, "Description", 9, 9, 14, 14, participants);
+        //    System.Drawing.Point p = new System.Drawing.Point(x, y);
+        //    Act.convertionCoordonneesXMLVersImage();
+        //}
+
+        //////////////////////////////////// Test Classe Planning ////////////////////////////////
+
+        //public void Test_Planning_Planning()
+        //{ 
+            
+        //}
+
+
+
     }
 }
