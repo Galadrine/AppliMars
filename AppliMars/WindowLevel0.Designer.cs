2d32e5132995cf44461ddf766a21d0eb7aae4722
LydieVH <lvanhoevela@ensc.fr>
2015-12-17 23:18:43 +0100

    Aïe

--- AppliMars/WindowLevel0.Designer.cs
+++ AppliMars/WindowLevel0.Designer.cs
@@ -99,6 +99,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.ClientSize = new System.Drawing.Size(864, 593);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
